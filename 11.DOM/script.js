7b0b59cc34cfc36d1b82f0ac942195f888008966
nautiyaladitya <nautiyaladitya011@gmail.com>
2024-05-29 02:31:09 -0700

    Initial commit

---
+++ 11.DOM/script.js
@@ -0,0 +1,30 @@
+
+//        DOM Attributes !!
+
+let div = document.querySelector("div");
+console.log(div);
+
+console.log(div.getAttribute("class"));   // to get the attribute (here class) of an element !
+          //OR
+// let cls = div.getAttribute("class");
+// console.log(cls);
+
+let p = document.querySelector("p");
+console.log(p);
+
+console.log(p.setAttribute("class","newPara")); // (att.(eg class /id) , value) to set/change the attribute value !
+                                                 // changed the classs name from para to newPara
+
+
+ let box = document.querySelector(".box");
+ div.style.backgroundColor="purple";   // style -> changing the css dynamically !!
+
+ div.style.fontSize="30px";
+
+ div.innerText="I'm the DIV";
+
+ div.style.visibility="hidden";
+ let idvs = document.querySelector(".box");
+console.log(divs.textContent);
+
+
